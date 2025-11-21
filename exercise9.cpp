872488d5a789126cef26da2ab6a1d7e3701e0379
PavlinaAndonova <pavlina.andonova13@gmail.com>
2025-11-21 11:05:06 +0000

    Add files via upload

---
+++ exercise9.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // Include the iostream library for input and output    
+using namespace std;    // Use the standard namespace
+
+// Function definition 
+void hello() {  //  Function name: hello
+    cout << "Hello" << endl;    // Print "Hello" to the console
+}
+
+int main() {    // Main function
+    hello();  // Function call
+    return 0;   // Return 0 to indicate successful execution
+}
