ed3c965b7d6dffaa3fccbcbce7c254f124e57a5d
PavlinaAndonova <pavlina.andonova13@gmail.com>
2025-11-21 12:47:44 +0000

    Delete exercise1.cpp

--- exercise1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream> // Include the iostream library for input and output   
-#include <string> // Include the string library for string manipulation
-using namespace std; // Use the standard namespace
-int main() { // Main function - execution starts here
-    cout << "Coding is Cool" << endl; // Display message to the user
-    return 0; // Indicate that the program ended successfully
-}
